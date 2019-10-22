dad0a54934d7fc19500c9a3a9b9ae15ac8b81fe5
Tim Carlson <timca@dashplatform.com>
2019-10-22 11:33:07 -0700

    pre-lecture DOM commit

---
+++ DOM/js/index.js
@@ -0,0 +1,80 @@
+'use strict';
+
+// // Slide 9 - (example 1)
+// let theH1 = document.querySelector('h1');
+// console.log(theH1)
+
+// //change content
+// let subtitle = document.querySelector('header p');
+// subtitle.innerHTML = subtitle.textContent + " <em>Prof Carlson</em>";
+
+
+//  //change attributes - SLIDE 10 (example 2)
+//  let img = document.querySelector('img');
+//  img.src = 'img/husky.jpg';
+
+// //change style - SLIDE 11 (example 3)
+// theH1.classList.add('text-success');
+// img.classList.add('anim')
+
+// //create an element -- SLIDE 12 (example 4)
+// let newLi = document.createElement('li')
+// newLi.innerHTML = '<a href="https://ischool.uw.edu/">Another iSchool Link</a>'
+// let linkList = document.querySelector('ul')
+// linkList.appendChild(newLi)
+
+// //USE Function to add item(s) - SLIDE 13 (example 5)
+// function renderCookie() {
+//     let jar = document.querySelector('#cookie-jar');
+//     let cookie = document.createElement('img')
+//     cookie.src = "img/cookie.png"
+//     cookie.alt = "a cookie"
+//     jar.appendChild(cookie);
+// }
+
+// renderCookie();
+
+
+// // USE Function to add n number by iterating - SLIDE 13 (example 5 continued)
+// function renderCookieJar(number) {
+//     let jar = document.querySelector('#cookie-jar')
+//     jar.innerHTML = '';             // deleting any cookies that used to be there
+//     for(let i=0; i<number; i++){
+//         renderCookie()
+//     }
+// }
+
+// //actually do that
+// renderCookieJar(10);
+
+
+//  renderCookieJar(5);
+
+// // Add Event Listener - SLIDE 15 (example 6)
+
+// let button1 = document.querySelector('#button1');  
+// button1.addEventListener('click', function() {      //register this callback inline function
+// console.log("button clicked")
+
+// });
+
+// let isPuppy = true;     //global variable
+
+// img.addEventListener('click', function() {
+//     if(isPuppy){
+//         img.src = 'img/husky.jpg';
+//         isPuppy = false
+//     } else {
+//         img.src = 'img/puppy.jpg';
+//         isPuppy = true
+//     }
+// })
+
+
+// // Temporary local data store
+// let DATA = {
+//     username: "tim",
+//     cookietype: "choclate chip",
+
+// }
+
