dad9ced92a87ef14706c4fc17eabf4cbb3c19054
Tim Carlson <timca@dashplatform.com>
2019-10-22 17:02:28 -0700

    updates for DOM lecture

--- DOM/js/index.js
+++ DOM/js/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// // Slide 9 - (example 1)
+// // Slide 8 - (example 1)
 // let theH1 = document.querySelector('h1');
 // console.log(theH1)
 
@@ -9,21 +9,27 @@
 // subtitle.innerHTML = subtitle.textContent + " <em>Prof Carlson</em>";
 
 
-//  //change attributes - SLIDE 10 (example 2)
+//  //change attributes - SLIDE 9 (example 2)
 //  let img = document.querySelector('img');
 //  img.src = 'img/husky.jpg';
 
-// //change style - SLIDE 11 (example 3)
+// // //change style - SLIDE 10 (example 3)
 // theH1.classList.add('text-success');
-// img.classList.add('anim')
+// img.classList.add('anim');
 
-// //create an element -- SLIDE 12 (example 4)
+// // in console add this : img.classList.toggle('scale-out')
+
+
+
+// //create an element -- SLIDE 11 (example 4)
 // let newLi = document.createElement('li')
 // newLi.innerHTML = '<a href="https://ischool.uw.edu/">Another iSchool Link</a>'
 // let linkList = document.querySelector('ul')
 // linkList.appendChild(newLi)
 
-// //USE Function to add item(s) - SLIDE 13 (example 5)
+
+
+// //USE Function to add item(s) - SLIDE 12 (example 5)
 // function renderCookie() {
 //     let jar = document.querySelector('#cookie-jar');
 //     let cookie = document.createElement('img')
@@ -35,7 +41,7 @@
 // renderCookie();
 
 
-// // USE Function to add n number by iterating - SLIDE 13 (example 5 continued)
+// // USE Function to add n number by iterating - SLIDE 12 (example 5 continued)
 // function renderCookieJar(number) {
 //     let jar = document.querySelector('#cookie-jar')
 //     jar.innerHTML = '';             // deleting any cookies that used to be there
@@ -50,7 +56,7 @@
 
 //  renderCookieJar(5);
 
-// // Add Event Listener - SLIDE 15 (example 6)
+// // Add Event Listener - SLIDE 14 (example 6)
 
 // let button1 = document.querySelector('#button1');  
 // button1.addEventListener('click', function() {      //register this callback inline function
@@ -58,7 +64,9 @@
 
 // });
 
-// let isPuppy = true;     //global variable
+// // Keep state as events occur - SLIDE 15 (example 7)
+
+// let isPuppy = false;     //global variable
 
 // img.addEventListener('click', function() {
 //     if(isPuppy){
